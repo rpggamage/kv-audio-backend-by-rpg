11929367b4a9df25a074aba6ffec05f06808ee7d
rpggamage@gmail.com <rpggamage@gmail.com>
2025-01-05 14:25:01 +0530

    I rtested different http request

--- index.js
+++ index.js
@@ -2,8 +2,27 @@ import express from "express";
 
 let app = express();
 
+app.get("/", (req, res) => {
+    console.log("That is a get request");
+    res.send("You send me a get request");
+}
+);
+app.post("/", (req, res) => {
+    console.log("That is a post request");
+    res.send("You send me a post request");
+})
+
+app.delete("/", (req, res) => {
+    console.log("That is a delete request");
+    res.send("You send me a delete request");
+})
+app.put("/", (req, res) => {
+    console.log("That is a put request");
+    res.send("You send me a put request");
+})
 app.listen(3000, () => {
     console.log("Server is running on port 3000");
-});
+}
+);
 
 // console.log("Hello World");
