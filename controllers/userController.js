e84f08bf222de2bbd9b3921400e89ca293943edf
rpggamage@gmail.com <rpggamage@gmail.com>
2025-01-13 13:06:12 +0530

    just before make it public 😂👍💕

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,12 +1,14 @@
 import User from "../models/user.js";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
-
+import dotenv from "dotenv";
+dotenv.config();
 export function registerUser(req, res) {
 
     const data = req.body;
     data.password = bcrypt.hashSync(data.password, 10);
     const newUser = new User(data);
+
 
     newUser.save()
         .then(() => {
